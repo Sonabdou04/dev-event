3b113ab4a2ad03af6996b95977117ca815cf038e
Abdou Peace <abdslamleo10@gmail.com>
2025-11-10 09:01:44 +0100

    Add styles & important dependencies

---
+++ app/(dashboard)/dashboard/page.jsx
@@ -0,0 +1,9 @@
+
+export default function page() {
+  // throw new Error("Something went wrong");
+  return (
+    <div>
+      Dashboard
+    </div>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <section>
+      <h1 className='text-center'>The Hub of Every Dev <br /> Do Not Miss Any Event With Us</h1>
+      <p className='text-center mt-5'>Meetups, Hackathons, Workshops, and More</p>
+    </section>
+  )
+}
